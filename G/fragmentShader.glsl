437cba160a4565d17caf79ea66a212b71fabc263
Yamin Ahmed <Yamin.Ahmed@outlook.de>
2025-02-23 19:36:37 +0100

    add comments

--- G/fragmentShader.glsl
+++ G/fragmentShader.glsl
@@ -2,9 +2,11 @@
 out vec4 FragColor;  
 in vec3 ourColor;
 
-uniform float offset1;
-uniform float offset2;
-uniform float offset3;
+uniform float offset1; // uniform offset values
+uniform float offset2; // the reason why its a uniform is because we want to change it from the CPU
+uniform float offset3; // also we do not have a function implememnted called Uniform3f or 4f or fv to change a unifrom like that since thats missing in
+// in the header file so it has to be done like this in the first place also i dont have time right now i will do it later for the render engine
+// and when i have the PC it will be blazingly fast
 
 void main()
 {
