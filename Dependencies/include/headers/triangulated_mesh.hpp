330daba74fe458027531e76d6834f1ef6aa629bf
Yamin Ahmed <yaminahmed821@gmail.com>
2025-04-11 19:32:11 +0200

    saving triangulated mesh loader

--- Dependencies/include/headers/triangulated_mesh.hpp
+++ Dependencies/include/headers/triangulated_mesh.hpp
@@ -4,13 +4,15 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <string>
+
 // This file loads basic .obj files and sets them up for opengl (must be triangulated)
 struct vertex{
     float x,y,z;
 };
 
 namespace triangulated_mesh{
-    
+    void getVertexBufferAndIndeciesBufferFromObjectFile(std::string path, struct vertex* vertexBuffer, int* indecies);
 }
 
 
--- src/implementation/triangulated_mesh.cpp
+++ src/implementation/triangulated_mesh.cpp
@@ -1,2 +1,43 @@
 #include <headers/triangulated_mesh.hpp>
+#include <fstream>
 
+
+static int countSubStringAmount(std::ifstream& stream, std::string substring)
+{
+    int count = 0;
+    std::string line;
+
+    while(std::getline(stream, line))
+    {
+        if(line.find(substring)!= std::string::npos)
+        {
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+
+void triangulated_mesh::getVertexBufferAndIndeciesBufferFromObjectFile(std::string path, struct vertex* vertexBuffer, int* indecies)
+{
+    int vAmount;
+    int fAmount;
+
+    {
+        std::ifstream file(path);
+        vAmount = countSubStringAmount(file, "v ");
+    }
+    {
+        std::ifstream file(path);
+        fAmount = countSubStringAmount(file, "f ");
+    }
+
+    //create Vertex Buffer
+    struct vertex* vBuffer = static_cast<struct vertex*>(malloc(sizeof(struct vertex)* vAmount));
+    int* fBuffer = static_cast<int*>(malloc(sizeof(int)*fAmount));
+    
+    
+
+
+}
