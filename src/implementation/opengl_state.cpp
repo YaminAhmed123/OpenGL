294d5140273126da03b7e508c488c1249396f9f4
Yamin Ahmed <yaminahmed821@gmail.com>
2025-04-15 18:14:41 +0200

    the lib was calles portable file dialog not the one in the commit before

--- src/implementation/opengl_state.cpp
+++ src/implementation/opengl_state.cpp
@@ -3,8 +3,6 @@
 
 // implements the namepsace "state" from the included header
 
-
-
 // use this to create a Shader based on Path and write into a shader object
 void opengl::state::createShader(Shader* shader, std::string vPath, std::string fPath)
 {
